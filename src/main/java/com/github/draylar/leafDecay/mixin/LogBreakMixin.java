47d578554213d58930be1e054d7c333ce66d2eac
Draylar <samuelhimes@gmail.com>
2019-05-29 15:03:54 -0500

    documenting

--- src/main/java/com/github/draylar/leafDecay/mixin/LogBreakMixin.java
+++ src/main/java/com/github/draylar/leafDecay/mixin/LogBreakMixin.java
@@ -14,11 +14,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+/**
+ * Helps kick-start leaf decay process by decaying the 4 leaves around a log that's broken if it's at the top of a tree.
+ */
 @Mixin(Block.class)
 public class LogBreakMixin
 {
     @Inject(at = @At("TAIL"), method = "onBreak")
-    public void afterBreak(World world_1, BlockPos blockPos_1, BlockState blockState_1, PlayerEntity playerEntity_1, CallbackInfo ci)
+    private void afterBreak(World world_1, BlockPos blockPos_1, BlockState blockState_1, PlayerEntity playerEntity_1, CallbackInfo ci)
     {
         if(blockState_1.getBlock() instanceof LogBlock)
         {
--- src/main/java/com/github/draylar/leafDecay/scheduler/LeafBreakHandler.java
+++ src/main/java/com/github/draylar/leafDecay/scheduler/LeafBreakHandler.java
@@ -2,15 +2,6 @@ package com.github.draylar.leafDecay.scheduler;
 
 import com.github.draylar.leafDecay.util.LeavesBreaker;
 import net.fabricmc.fabric.api.event.server.ServerTickCallback;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.fluid.FluidState;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 import java.util.ArrayList;
 
